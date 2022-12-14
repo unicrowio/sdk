d295ced141995d7bdbd3a4a2cc5590f9bbbd2293
Thiago Marinho <tgmarinho@gmail.com>
2022-12-14 15:15:52 -0400

    chore: make others contract address available to SDK, remove some unused contract address and comment the impl of getEscrowEarningTokens function

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,19 +1,43 @@
 import { networks } from '../wallet/networks'
 import initNetworks from './init'
 
-export const RPC_HOST =
+export let RPC_HOST =
   globalThis?.defaultNetwork?.rpcUrl || networks?.arbitrum?.rpcUrls[0]
 
-export const UNICROW_ADDRESS = '0x1e8d308cA9b8098cB2D9EfBf54992B252f7CaAc0'
-export const UNICROW_DISPUTE_ADDRESS =
-  '0xA60b443FAef64ce299C31671a180e0309d6619D1'
-export const UNICROW_ARBITRATOR_ADDRESS =
-  '0x0Ae4B7FA978e3A21eb05444533d5744e02c9E095'
-export const CROW_REWARDS_ADDRESS = '0xfC5cac9F4f8f286bA227402060786Cb20A13C704'
-export const CROW_STAKING_ADDRESS = '0x1494A9b31ED5f4Ba7B993A7E3a6374627841bC9E'
-export const CROW_TOKEN_ADDRESS = '0xcce514eCBc99dbeaA2a809dCc137B57524fd5aaE'
-export const CROW_LIST_TOKENS = '0x2ec213411e2D31AE0e8b514b869F1d66951Ca1a0'
-export const UNICROW_CLAIM_ADDRESS = '0x85bD5aC27e86DD74ef5e9dba6dc5D2DF732bAE67'
+// Arbitrum mainnet Unicrow Contract Addresses
+const Arbitrum = {
+  unicrow: '0xFEf5DA5c801c137632D51d4ccec16e9a89A91deC',
+  dispute: '0xE5758Fe00EebFA200cE0e1e8818eedC6b8101aDb',
+  arbitrator: '0x6c3c223F8b5430b6E8D1bC2D2F2377503AC6Ffb8',
+  claim: '0xdDfDCa767F9143B804740aFA8087A286E087dF13'
+}
+
+// Private RPC development Unicrow Contract Addresses
+const Private = {
+  unicrow: '0x1e8d308cA9b8098cB2D9EfBf54992B252f7CaAc0',
+  dispute: '0xA60b443FAef64ce299C31671a180e0309d6619D1',
+  arbitrator: '0x0Ae4B7FA978e3A21eb05444533d5744e02c9E095',
+  claim: '0x85bD5aC27e86DD74ef5e9dba6dc5D2DF732bAE67'
+}
+
+export let UNICROW_ADDRESS =
+  globalThis.defaultNetwork.name == 'arbitrum'
+    ? Arbitrum.unicrow
+    : Private.unicrow
+export let UNICROW_DISPUTE_ADDRESS =
+  globalThis.defaultNetwork.name == 'arbitrum'
+    ? Arbitrum.dispute
+    : Private.dispute
+export let UNICROW_ARBITRATOR_ADDRESS =
+  globalThis.defaultNetwork.name == 'arbitrum'
+    ? Arbitrum.arbitrator
+    : Private.arbitrator
+
+export let UNICROW_CLAIM_ADDRESS =
+  globalThis.defaultNetwork.name == 'arbitrum' ? Arbitrum.claim : Private.claim
+
+// Not been used yet
+export let CROW_LIST_TOKENS = ''
 
 initNetworks({
   autoSwitchNetwork: false,
