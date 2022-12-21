14d1fb5457a6a06483e024930efe6183ce23bc0a
Emanuel Ferreira <contatoferreirads@gmail.com>
2022-12-21 17:24:11 -0300

    chore: remove console.log

--- src/helpers/ensToAddress.ts
+++ src/helpers/ensToAddress.ts
@@ -3,11 +3,6 @@ import { ethers } from 'ethers'
 
 export const ensToAddress = async (ensName: string) => {
     const provider = new ethers.providers.JsonRpcProvider("https://purple-distinguished-vineyard.quiknode.pro/360bd2d54ace2ca5b775f1bf8325875fcd77204f/", 'mainnet')
-
-    console.log({ ensName })
-
     const address = await provider.resolveName(ensName)
-
-    console.log({ x: address })
     return address
 }
