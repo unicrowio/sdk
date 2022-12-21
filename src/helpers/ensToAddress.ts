d7275f23e9f0187b43de610a537636043e60c505
Emanuel Ferreira <contatoferreirads@gmail.com>
2022-12-21 17:45:01 -0300

    chore: moved node to a variable

--- src/helpers/ensToAddress.ts
+++ src/helpers/ensToAddress.ts
@@ -1,8 +1,9 @@
 import { ethers } from 'ethers'
 
+const MAINNET_NODE = "https://purple-distinguished-vineyard.quiknode.pro/360bd2d54ace2ca5b775f1bf8325875fcd77204f/";
 
 export const ensToAddress = async (ensName: string) => {
-    const provider = new ethers.providers.JsonRpcProvider("https://purple-distinguished-vineyard.quiknode.pro/360bd2d54ace2ca5b775f1bf8325875fcd77204f/", 'mainnet')
+    const provider = new ethers.providers.JsonRpcProvider(MAINNET_NODE, 'mainnet')
     const address = await provider.resolveName(ensName)
     return address
 }
