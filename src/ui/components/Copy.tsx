0fe3834aae574a2f47dc61840f9e2c380b1ec40c
Pascal Weber <p.weber.92@gmail.com>
2022-12-22 12:25:04 -0500

    replace ClipboardJS with native function

--- src/ui/components/Copy.tsx
+++ src/ui/components/Copy.tsx
@@ -1,25 +1,34 @@
 import React, { ReactNode } from "react";
-import ClipboardJS from "clipboard";
 import { CopyIcon } from "../../ui/components/icons/Copy";
 import { toast } from "../../ui/components/notification/toast";
 
 type CopyToClipboardProps = {
-	content: string | ReactNode;
-	copy: string;
+  content: string | ReactNode;
+  copy: string;
 };
 export const CopyToClipboard = ({ content, copy }: CopyToClipboardProps) => {
-	React.useEffect(() => {
-		// eslint-disable-next-line no-new
-		new ClipboardJS(".span-clipboard");
-	}, []);
-	return (
-		<span
-			style={{ cursor: "pointer", height: "auto", width: "auto" }}
-			className="span-clipboard"
-			onClick={() => toast("Copied to the clipboard", "success")}
-			data-clipboard-text={copy}
-		>
-			{content} <CopyIcon />
-		</span>
-	);
+  return (
+    <span
+      style={{ cursor: "pointer", height: "auto", width: "auto" }}
+      className="span-clipboard"
+      onClick={async () => {
+        copyToClipboard(copy).then(() => {
+          toast("Copied to the clipboard", "success");
+        });
+      }}
+      data-clipboard-text={copy}
+    >
+      <>
+        {content} <CopyIcon />
+      </>
+    </span>
+  );
+};
+
+const copyToClipboard = async (text) => {
+  if ("clipboard" in navigator) {
+    return await navigator.clipboard.writeText(text);
+  } else {
+    return document.execCommand("copy", true, text);
+  }
 };
