c407f80346fb5247e360781b91e58450503d05a0
Pascal Weber <p.weber.92@gmail.com>
2023-01-17 09:18:45 -0500

    rome

--- src/ui/modals/Challenge.tsx
+++ src/ui/modals/Challenge.tsx
@@ -74,7 +74,7 @@ export function ChallengeModal(props: IChallengeModalProps) {
 
   const [paymentStatus, setPaymentStatus] = React.useState<
     string | undefined
-    >();
+  >();
 
   const {
     buttonLabel,
