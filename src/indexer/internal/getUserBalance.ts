f10bcf6807f9aa0c6e6bb74c7e3069bff109afe2
Thiago Marinho <tgmarinho@gmail.com>
2023-01-19 18:39:10 -0400

    refactor getUserBalance

--- src/indexer/internal/getUserBalance.ts
+++ src/indexer/internal/getUserBalance.ts
@@ -14,6 +14,7 @@ import {
 } from "../../typing";
 import { buildBalanceQuery } from "./queryBalance";
 import BigNumber from "bignumber.js";
+import { parseEscrowData } from "./parseEscrowData";
 
 const addTokenInfo = async (balances: IBalance[]) => {
   const tokens: IToken[] = [];
@@ -25,7 +26,7 @@ const addTokenInfo = async (balances: IBalance[]) => {
 };
 
 const prepareResponseData = (balance: any, tokens: IToken[]) => {
-  const _amount = new BigNumber(balance.amount).div(100);
+  const _amount = new BigNumber(balance.amount);
   const tokenInfo = tokens.find((t) => t.address === balance.token.address);
 
   return {
@@ -37,7 +38,9 @@ const prepareResponseData = (balance: any, tokens: IToken[]) => {
   };
 };
 
-
+/*
+  Get balance from logged user
+*/
 export const getUserBalance = async (
   client: GraphQLClient,
   walletUserAddress: string,
@@ -50,9 +53,14 @@ export const getUserBalance = async (
   }>(queryString);
 
   const { pending, ready_for_claim } = response;
+  const parsedPending = pending.map(parseEscrowData);
+  const parsedReady = ready_for_claim.map(parseEscrowData);
 
-  const groupPendingByToken = groupBy(pending, (item) => item.currency);
-  const groupReadyByToken = groupBy(ready_for_claim, (item) => item.currency);
+  const groupPendingByToken = groupBy(
+    parsedPending,
+    (item) => item.token.address,
+  );
+  const groupReadyByToken = groupBy(parsedReady, (item) => item.token.address);
 
   const pendingData: IBalance[] = Object.keys(groupPendingByToken)
     .map((key) => {
@@ -86,6 +94,8 @@ export const getUserBalance = async (
 
   return {
     pending: pendingData.map((balance) => prepareResponseData(balance, tokens)),
-    readyForClaim: readyData.map((balance) => prepareResponseData(balance, tokens)),
+    readyForClaim: readyData.map((balance) =>
+      prepareResponseData(balance, tokens),
+    ),
   };
 };
