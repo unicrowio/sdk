ece6a5bb0cf6c284f9acdb31257f8c627d525303
Thiago Marinho <tgmarinho@gmail.com>
2023-02-09 11:45:16 -0400

    amends

--- src/core/getBalance.ts
+++ src/core/getBalance.ts
@@ -11,7 +11,7 @@ import { BigNumber } from "ethers";
  * Checks if the user has the funds to pay for the given amount of tokens.
  * This function should be called only to verify the users' balance of ERC20 tokens.
  *
- * @param tokenAddress - (optional or null for ETH) -Address of the token to check the user's balance of. Set to 0 address to check for ETH
+ * @param tokenAddress - Address of the token to check the user's balance of. (null for ETH)
  * @returns User's balance as BigNumber of the given ETH's or token's WEI
  */
 export const getBalance = async (
@@ -26,7 +26,7 @@ export const getBalance = async (
 
   autoSwitchNetwork();
 
-  if (!tokenAddress || tokenAddress === ETH_ADDRESS) {
+  if (tokenAddress === ETH_ADDRESS) {
     return provider.getBalance(currentAccount);
   } else {
     const token = ERC20__factory.connect(tokenAddress, provider.getSigner());
