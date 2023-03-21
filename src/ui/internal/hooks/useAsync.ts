959d221a055334fdab438872341d7c1a87de8d47
Thiago Marinho <tgmarinho@gmail.com>
2023-03-21 09:41:52 -0400

    refactor useAsync and add some comments for TODO

--- src/ui/internal/hooks/useAsync.ts
+++ src/ui/internal/hooks/useAsync.ts
@@ -3,21 +3,31 @@ import useSWR, { mutate } from "swr";
 import { useNetworkCheck } from "../hooks/useNetworkCheck";
 import { toast } from "../notification/toast";
 
+// TODO use typescript properly
+// interface IUseAsync {
+//   data: any;
+//   error: any;
+//   isLoading: boolean;
+// }
+
+// TODO Crate a custom hook for each fecther or data instead of generic one.
+
 export const useAsync = (
-  args,
-  fn,
-  onModalClose?,
-  defaultValue?,
-  noRefresh?,
+  args: any,
+  fetcher: (args?: any) => Promise<any>,
+  onModalClose?: VoidFunction,
+  defaultValue?: any,
+  shouldRefresh = true,
 ) => {
   const { isCorrectNetwork } = useNetworkCheck();
 
   const { data, isLoading, error } = useSWR(
     isCorrectNetwork ? args : null,
-    fn,
-    noRefresh ? {} : { refreshInterval: 1000 },
+    fetcher,
+    shouldRefresh ? {} : { refreshInterval: 1000 },
   );
 
+  // TODO remove this useEffect
   React.useEffect(() => {
     if (error) {
       toast.error(error);
@@ -25,6 +35,7 @@ export const useAsync = (
     }
   }, [error]);
 
+  // TODO return object instead of array
   return [error ? null : data || defaultValue, isLoading, error];
 };
 
